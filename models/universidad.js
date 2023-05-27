aeb0d72b03b885c8d4d8ea08f9bd1d7ce3ed9a44
OXKR96 <128667745+OXKR96@users.noreply.github.com>
2023-05-26 20:19:33 -0500

    Add files via upload

---
+++ models/universidad.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose')
+
+const UniversidadSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'Nombre requerido']
+        
+    },
+    direccion: {
+        type: String,
+        required: [true, 'Direccion requerida']
+        
+    },
+    telefono: {
+        type: String,
+        required: [true, 'Numero Tel requerido']
+        
+    },
+    fechaCreacion: {
+        type: Date,
+        default: new Date()
+    },
+    fechaActualizacion: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+module.exports = model('Universidad', UniversidadSchema)
